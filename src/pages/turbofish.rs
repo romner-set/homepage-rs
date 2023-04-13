e6795c20382548ec97921c6731283deba7e47315
romner-set <roman@skotnica.com>
2023-04-13 14:07:19 +0200

    Rewrite syntax highlighting

--- src/pages/turbofish.rs
+++ src/pages/turbofish.rs
@@ -1,6 +1,14 @@
 use rand::Rng;
 use crate::*;
 
+const HIGHLIGHTED_STRINGS: &[&str] = &["&mut"];
+const HIGHLIGHTED_CHARS: &str = "&<>()[];,";
+const HIGHLIGHT_EXCEPTIONS: &[&str] = &["()"]; // Don't highlight unit type
+
+enum MatchType {
+    Exact, StartsWith, None
+}
+
 #[derive(Properties, PartialEq)]
 pub struct TurbofishProps {
     #[prop_or_default]
@@ -25,55 +33,43 @@ pub fn Turbofish(props: &TurbofishProps) -> Html {
                         }>
                             <turbofish>
                             if !props.guts.is_empty() {{{
-                                    let mut mutref_buffer = String::new();
-                                    let clear_mutrefb = |mb: &mut String, offset: usize| -> Html {
-                                        html! {
-                                            <>
-                                                {{
-                                                    mb.drain(..mb.len()-offset).map(|_c| if _c=='&' {
-                                                        html! {<span>{'&'}</span>}
-                                                    } else {
-                                                        html! {_c}
-                                                    }).collect::<Html>()
-                                                }}
-                                            </>
-                                        }
-                                    };
-                                    props.guts.chars().map(|c| {
-                                        match c { // I realize how ugly this code is, but it is currently 
-                                                  // almost 2 AM and I am too sleep-deprived to care
-                                            '&' => {
-                                                mutref_buffer.push(c);
-                                                if mutref_buffer.len()==2 {html!{}}
-                                                else {clear_mutrefb(&mut mutref_buffer,1)}
+                                let mut buf = String::new();
+
+                                html! {for props.guts.chars().map(|c| {
+                                    buf.push(c);
+
+                                    let mut match_type = MatchType::None;
+                                    HIGHLIGHTED_STRINGS.iter().for_each(|&s|
+                                        if s==&buf {match_type = MatchType::Exact; return}
+                                        else if s.starts_with(&buf) {match_type = MatchType::StartsWith}
+                                    );
+
+                                    match match_type {
+                                        MatchType::None => {
+                                            HIGHLIGHT_EXCEPTIONS.iter().for_each(|&s|
+                                                if s==&buf {match_type = MatchType::Exact; return}
+                                                else if s.starts_with(&buf) {match_type = MatchType::StartsWith}
+                                            );
+                                            match match_type {
+                                                MatchType::Exact => html!{for buf.drain(..)},
+                                                MatchType::StartsWith => html!{},
+                                                MatchType::None => html! {
+                                                    for buf.drain(..).map(|_c| {
+                                                        if HIGHLIGHTED_CHARS.contains(_c) {
+                                                            html!{<span>{_c}</span>}
+                                                        } else {
+                                                            html!{_c}
+                                                        }
+                                                    })
+                                                }
                                             }
-                                            'm' => {
-                                                mutref_buffer.push(c);
-                                                if mutref_buffer.len()==2 {html!{}}
-                                                else {clear_mutrefb(&mut mutref_buffer,0)}
-                                            }
-                                            'u' => {
-                                                mutref_buffer.push(c);
-                                                if mutref_buffer.len()==3 {html!{}}
-                                                else {clear_mutrefb(&mut mutref_buffer,0)}
-                                            }
-                                            't' => {
-                                                mutref_buffer.push(c);
-                                                if mutref_buffer.len()==4 {
-                                                    html! {<span>{mutref_buffer.drain(..).collect::<String>()}</span>}
-                                                } else {clear_mutrefb(&mut mutref_buffer,0)}
-                                            }
-                                            c => html! {
-                                                <>
-                                                    if !mutref_buffer.is_empty() {{clear_mutrefb(&mut mutref_buffer,0)}}
-                                                    {match c {
-                                                        '<'|'>'|','|'['|']'|';' => html! {<span>{c}</span>},
-                                                        c => html! {c},
-                                                    }}
-                                                </>
-                                            }
-                                        }
-                                    }).collect::<Html>()
+                                        },
+                                        MatchType::Exact => html! {
+                                            html!{<span>{for buf.drain(..)}</span>}
+                                        },
+                                        MatchType::StartsWith => html!{}
+                                    }
+                                })}
                             }}}
                             </turbofish>
                         </@>
