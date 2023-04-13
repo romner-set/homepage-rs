4f1422ab336b7faadd63e8d7c8f1afb6fab620f8
romner-set <roman@skotnica.com>
2023-04-13 12:31:24 +0200

    Add arrays and refs to random

--- src/random.rs
+++ src/random.rs
@@ -1,4 +1,4 @@
-use rand::{prelude::ThreadRng, seq::SliceRandom};
+use rand::{prelude::ThreadRng, seq::SliceRandom, Rng};
 
 struct TypeDef {
     name: &'static str,
@@ -11,7 +11,8 @@ impl TypeDef {
 }
 
 const RECURSION_LIMIT: u8 = 1;
-const ARRAY_CHANCE: f64 = 0.1;
+const REF_CHANCE: f64 = 0.2;
+const ARRAY_CHANCE: f64 = 0.05;
 const TYPES: &[TypeDef] = &[
     TypeDef::new("_", 0),
     TypeDef::new("bool", 0),
@@ -47,17 +48,35 @@ pub fn random_type(rng: &mut ThreadRng, recursion_depth: u8) -> String {
         }
     }
 
-    let mut generics = String::new();
+    let mut ret = String::new();
+    let is_array = ARRAY_CHANCE > rng.gen();
+
+    let mut ref_add = |r: &mut String| {
+        if REF_CHANCE > rng.gen() {r.push('&')}
+        else if REF_CHANCE > rng.gen() {r.push_str("&mut ")}
+    };
+
+    ref_add(&mut ret);
+    if is_array {
+        ret.push_str(&format!("["));
+        ref_add(&mut ret);
+    }
+
+    ret.push_str(rtype.name);
 
     if rtype.generics > 0 {
-        generics.push('<');
-        for _ in 0..(rtype.generics-1) {
-            generics.push_str(&random_type(rng, recursion_depth+1));
-            generics.push_str(", ");
+        ret.push('<');
+        for _ in 0..rtype.generics {
+            ret.push_str(&random_type(rng, recursion_depth+1));
+            ret.push_str(", ");
         }
-        generics.push_str(&random_type(rng, recursion_depth+1));
-        generics.push('>');
+        ret.push_str(&random_type(rng, recursion_depth+1));
+        ret.push('>');
     }
-    
-    format!("{}{generics}", rtype.name)
+
+    if is_array {
+        ret.push_str(&format!("; {}]", rng.gen::<u8>()));
+    }
+
+    ret
 }
